6e73522ece64e455278340332e830a06a4cda8f9
Tassilo Weidner <tassilo.weidner@camunda.com>
2021-04-28 11:13:58 +0200

    test(qa/engine): add failing test
    Batch set variable: Object variable cannot be deserialized since type is only present process application.
    
    related to CAM-13435

--- qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/util/AbstractFoxPlatformIntegrationTest.java
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/util/AbstractFoxPlatformIntegrationTest.java
@@ -24,11 +24,16 @@ import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.jobexecutor.JobExecutor;
 import org.camunda.bpm.engine.impl.util.ClockUtil;
 import org.camunda.bpm.engine.runtime.Job;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -171,6 +176,14 @@ public abstract class AbstractFoxPlatformIntegrationTest {
       timeLimitExceeded = true;
       thread.interrupt();
     }
+  }
+
+  protected static Asset modelAsAsset(BpmnModelInstance modelInstance) {
+    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    Bpmn.writeModelToStream(byteStream, modelInstance);
+
+    byte[] bytes = byteStream.toByteArray();
+    return new ByteArrayAsset(bytes);
   }
 
 }
